f7a75057988738ec8a847b30382532238dc7590f
Martin Gotink <m.gotink@gmail.com>
2021-12-03 21:13:41 +0100

    Remove duplicate logic for day 3, part 2

--- day3/3b.rb
+++ day3/3b.rb
@@ -1,33 +1,25 @@
+def find_value(lines, inverted=false)
+  comparison_method = inverted ? :<= : :>=
+  default_value = inverted ? "0" : "1"
+  other_value = inverted ? "1" : "0"
+
+  for index in 0...lines[0].size do
+    count = lines.transpose[index].count(default_value)
+
+    matching_bit = (count * 2).send(comparison_method, lines.size) ? default_value : other_value
+
+    lines = lines.filter { |line| line[index] == matching_bit || lines.size == 1 }
+  end
+
+  lines[0].join.to_i(2)
+end
+
 lines = File.readlines("3.txt", chomp: true)
   .map { |line| line.split("") }
 
-oxygen_lines = lines.clone
-for index in 0...12 do
-  count = oxygen_lines
-    .map { |bits| bits[index] }
-    .count("1")
+oxygen_generator_rating = find_value(lines)
 
-  most_common_bit = count * 2 >= oxygen_lines.size ? "1" : "0"
-
-  oxygen_lines
-    .filter! { |line| line[index] == most_common_bit }
-end
-
-oxygen_generator_rating = oxygen_lines[0].join.to_i(2)
-
-co2_lines = lines.clone
-for index in 0...12 do
-  count = co2_lines
-    .map { |bits| bits[index] }
-    .count("0")
-
-  least_common_bit = count * 2 <= co2_lines.size ? "0" : "1"
-
-  co2_lines
-    .filter! { |line| line[index] == least_common_bit || co2_lines.size == 1 }
-end
-
-co2_scrubber_rating = co2_lines[0].join.to_i(2)
+co2_scrubber_rating = find_value(lines, true)
 
 puts oxygen_generator_rating * co2_scrubber_rating
 
